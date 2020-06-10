616f7fee3d69282aba2607b358500cd0f0880d3c
John <johnfg2610@gmail.com>
2020-06-10 06:44:37 +0100

    added key support

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/rotasys/x
 go 1.14
 
 require (
-	github.com/gin-gonic/gin v1.6.3 // indirect
-	github.com/rs/zerolog v1.19.0 // indirect
-	github.com/square/go-jose v2.5.1+incompatible // indirect
-	github.com/uniplaces/carbon v0.1.6 // indirect
+	github.com/gin-gonic/gin v1.6.3
+	github.com/rs/zerolog v1.19.0
+	github.com/square/go-jose v2.5.1+incompatible
+	github.com/uniplaces/carbon v0.1.6
 	gopkg.in/square/go-jose.v2 v2.5.1 // indirect
 )
--- jwt_auth.go
+++ jwt_auth.go
@@ -1,14 +1,15 @@
 package x
 
 import (
-	"time"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
 
 	"github.com/gin-gonic/gin"
 	"github.com/square/go-jose/jwt"
 )
 
 type JWTConfig struct {
-	Claims            jwt.Claims
 	FailedAuthHandler AuthenticationFailedHandler
 	Header            string
 	Key               interface{}
@@ -16,17 +17,83 @@ type JWTConfig struct {
 
 type AuthenticationFailedHandler func(c *gin.Context, err error)
 
-func NewJWTConfig(sub string, iss string, aud jwt.Audience) JWTConfig {
+func NewJWTConfig() JWTConfig {
 	return JWTConfig{
-		Claims: jwt.Claims{
-			Subject:   sub,
-			Issuer:    iss,
-			NotBefore: jwt.NewNumericDate(time.Date(2016, 1, 1, 0, 0, 0, 0, time.UTC)),
-			Expiry:    jwt.NewNumericDate(time.Date(2016, 1, 1, 0, 15, 0, 0, time.UTC)),
-			Audience:  aud,
-		},
 		Header: "Authorization",
 	}
+}
+
+func loadPublicKey(data []byte) (interface{}, error) {
+	input := data
+
+	block, _ := pem.Decode(data)
+	if block != nil {
+		input = block.Bytes
+	}
+
+	// Try to load SubjectPublicKeyInfo
+	pub, err0 := x509.ParsePKIXPublicKey(input)
+	if err0 == nil {
+		return pub, nil
+	}
+
+	cert, err1 := x509.ParseCertificate(input)
+	if err1 == nil {
+		return cert.PublicKey, nil
+	}
+
+	jwk, err2 := LoadJSONWebKey(data, true)
+	if err2 == nil {
+		return jwk, nil
+	}
+
+	return nil, errors.New("parse error, invalid public key")
+}
+
+func loadPrivateKey(data []byte) (interface{}, error) {
+	input := data
+
+	block, _ := pem.Decode(data)
+	if block != nil {
+		input = block.Bytes
+	}
+
+	var priv interface{}
+	priv, err0 := x509.ParsePKCS1PrivateKey(input)
+	if err0 == nil {
+		return priv, nil
+	}
+
+	priv, err1 := x509.ParsePKCS8PrivateKey(input)
+	if err1 == nil {
+		return priv, nil
+	}
+
+	priv, err2 := x509.ParseECPrivateKey(input)
+	if err2 == nil {
+		return priv, nil
+	}
+
+	jwk, err3 := LoadJSONWebKey(input, false)
+	if err3 == nil {
+		return jwk, nil
+	}
+
+	return nil, errors.New("parse error, invalid private key")
+}
+
+func (j *JWTConfig) AddPublicKey(data []byte) error {
+	key, err := loadPublicKey(data)
+	if err == nil {
+		j.Key = key
+		return nil
+	}
+	key, err = loadPrivateKey(data)
+	if err == nil {
+		j.Key = key
+		return nil
+	}
+	return err
 }
 
 func (j *JWTConfig) Handler() gin.HandlerFunc {
